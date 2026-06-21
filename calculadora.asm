3b0b43e3a5903e590ac1464b045269a1742c8f7b
Withoutsleep <janaranda88@gmail.com>
2026-06-21 18:29:15 +0200

    Add files via upload
    Fix #2 - Just fixed error 3007 and also I deleted HacerResta and HacerMultiplicacion

--- calculadora.asm
+++ calculadora.asm
@@ -13,81 +13,73 @@
 VDP_CONTROL EQU $00C00004   ; Esto se utiliza para poder llamar de un determinado nombre una direccion en la memoria
 VDP_DATA    EQU $00C00000   
 
-move.w #0, ($00FF0010) ; Aqui dejaré el primer numero (que de momento es 30 pero a futuro se podra cambiar) ;Para restaurar los numeros
-move.w #0, ($00FF0012) ; Aqui el segundo numero
+    move.w #0, ($00FF0010) ; Aqui dejaré el primer numero (que de momento es 30 pero a futuro se podra cambiar) ;Para restaurar los numeros
+    move.w #0, ($00FF0012) ; Aqui el segundo numero
 
 EsperarBoton:
 
     move.b  #$40,($00A10009) ; Inicializa el mando
     move.b  ($00A10003),d0 ; Lee los botones reales
     andi.b  #%01000000,d0   ; Compara si has tocado el boton A
-    beq.s HacerSuma
-
-    move.b  ($00A10003),d0 ; Lee los botones reales
-    andi.b  #%00010000,d0   ; Compara si has tocado el boton B
-    beq.s HacerResta
-
-    move.b  ($00A10003),d0 ; Lee los botones reales
-    andi.b  #%00100000,d0   ; Compara si has tocado el boton C
-    beq.s HacerMultiplicacion
+    beq HacerSuma
 
     move.b  ($00A10003),d0 ; Lee los botones reales
     andi.b  #%00000001,d0   ; Compara si has tocado el boton UP
-    beq.s SubirNumero
+    beq SubirNumero
 
     move.b ($00A10003),d0 ; Lee los botones reales
     andi.b  #%00000010,d0  ; Comprueba si has tocado el boton DOWN
-    beq.s BajarNumero
+    beq BajarNumero
 
     move.b ($00A10003),d0 ; Lee los botones reales
     andi.b  #%00000100,d0  ; Comprueba si has tocado el boton LEFT
-    beq.s IzquierdearNumero
+    beq IzquierdearNumero
 
     move.b ($00A10003),d0 ; Lee los botones reales
     andi.b  #%00001000,d0  ; Comprueba si has tocado el boton RIGHT
-    beq.s DerechearNumero
+    beq DerechearNumero
 
-    bra.s EsperarBoton  ; Repite el ciclo
+    bra EsperarBoton  ; Repite el ciclo
 
 SubirNumero:
-    addq.w #1, ($00FF0010)
+    addq.w #1,($00FF0010)
 
 EsperarASoltarLaUP:
     move.b  ($00A10003),d0        ; Lee los botones 
     andi.b  #%00000001,d0
-    beq.s EsperarASoltarLaUP
+    beq EsperarASoltarLaUP
     bra.s EsperarBoton
 
 BajarNumero:
-    subq.w #1, ($00FF0010)
+    subq.w #1,($00FF0010)
 
 EsperarAsoltarLaDOWN:
     move.b  ($00A10003),d0        ; Lee los botones 
     andi.b  #%00000010,d0
-    beq.s EsperarASoltarLaDOWN
+    beq EsperarASoltarLaDOWN
     bra.s EsperarBoton
 
 IzquierdearNumero:
-    subq.w #1, ($00FF0012)
+    subq.w #1,($00FF0012)
 
 EsperarAsoltarLaLEFT:
     move.b  ($00A10003),d0        ; Lee los botones 
     andi.b  #%00000100,d0
-    beq.s EsperarAsoltarLaLEFT
+    beq EsperarAsoltarLaLEFT
     bra.s EsperarBoton
 
 DerechearNumero:
-    addq.w #1, ($00FF0012)
+    addq.w #1,($00FF0012)
 
-EsperarAsoltarLaLEFT:
+EsperarAsoltarLaRIGHT:
     move.b  ($00A10003),d0        ; Lee los botones 
     andi.b  #%00000100,d0
-    beq.s EsperarAsoltarLaRIGHT
+    beq EsperarAsoltarLaRIGHT
     bra.s EsperarBoton
 
 HacerSuma:
-    move.l #$C0000000, (VDP_CONTROL)  ; Es necesario llamar a esta funcion para poder "activar" el canal de control
-    move.w  #$082A, (VDP_DATA)
+    move.l #$C0000000,(VDP_CONTROL)  ; Es necesario llamar a esta funcion para poder "activar" el canal de control
+    move.w  #$082A,(VDP_DATA)
     move.w ($00FF0010),d0
     move.w ($00FF0012),d1
     add.w d0,d1
@@ -96,21 +88,8 @@ HacerSuma:
 EsperarASoltarLaA:
     move.b  ($00A10003),d0        ; Lee los botones 
     andi.b  #%01000000,d0
-    beq.s EsperarASoltarLaA
-    bne.s EsperarBoton
-
-HacerResta:
-    move.w #30,d0
-    move.w #20,d1
-    sub.w d0, d1
-    move.w d1,($00FF0000)
-    bra.s FinDelPrograma
-
-HacerMultiplicacion:
-    move.w #30,d0
-    move.w #20,d1
-    mulu.w d0,d1
-    move.w d1,($00FF0000)
+    beq EsperarASoltarLaA
+    bne EsperarBoton
 
 FinDelPrograma:
     bra.s FinDelPrograma ; Bucle final
